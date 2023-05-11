a20732f3c8c5570bc8ac0f3d38e78aa8fffc21d3
Bipasha-Singha <63922620+Bipasha-Singha@users.noreply.github.com>
2023-05-11 11:55:14 -0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink,  useNavigate } from 'react-router-dom';
 import cottonlogo from '../image/cottonlogo.png';
-//import { Navigate } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart} from '@fortawesome/free-solid-svg-icons';
 const Navbar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
   const navigate = useNavigate();
-  const handleLogout = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('userId');
-    navigate('/login');
-  }
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (searchTerm) {
+      navigate(`/search?q=${searchTerm}`);
+    } else {
+      navigate('/');
+    }
+    setSearchTerm('');
+  };
+  
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -39,9 +46,33 @@ const Navbar = () => {
         <NavLink className="nav-link" to="/categories">Categories</NavLink>
       </li>
       <li className="nav-item">
-        <NavLink className="nav-link" to="/logout" onClick={handleLogout}>Logout</NavLink>
+        <NavLink className="nav-link" to="/logout">Logout</NavLink>
       </li>
     </ul>
+    <ul className="navbar-nav">
+        <li className="nav-item">
+            <NavLink className="nav-link" to="/cart">
+               <FontAwesomeIcon icon={faShoppingCart} />
+            </NavLink>
+         </li>
+         <form className="form-inline my-2 my-lg-0" onSubmit={handleSearch}>
+          <div className="d-flex">
+            <input
+              className="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          <button className="btn btn-outline-success my-2 my-sm-0" type="submit" style={{height: "38px", width: "70px"}}>
+            Search
+          </button>
+
+          </div>
+        </form>
+    </ul>
+    
         </div>
       </nav>
     </>
