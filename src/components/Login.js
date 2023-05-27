01c3bad308641cd687359bdf8f428106d60db357
Bipasha-Singha <63922620+Bipasha-Singha@users.noreply.github.com>
2023-05-27 07:36:38 -0700

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,49 +1,53 @@
 import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import axios from 'axios';
-import { Navigate } from 'react-router-dom';
+import { Navigate, NavLink } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import {useGoogleLogin} from '@react-oauth/google';
-//import jwt_decode from 'jsonwebtoken';
+
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate = useNavigate();
-  //const expirationDate = new Date();
-  //expirationDate.setTime(expirationDate.getTime() + 24 * 60 * 60 * 1000); // Add 24 hours
-  
+
   const log = useGoogleLogin({
-    onSuccess: (coderesponse)=>{
+    onSuccess: (coderesponse) => {
       console.log(coderesponse.access_token);
       localStorage.setItem('accessToken', coderesponse.access_token);
-      navigate("/product");
+      navigate('/product');
     },
-    onError:(errorResponse)=>{}
-  })
+    onError: (errorResponse) => {
+      console.log(errorResponse);
+    },
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
 
-    axios.post("http://localhost:5000/api/v1/login", { email, password })
-    .then((response) => {
-      const { token, user } = response.data;
-       
-      // Set the token as a cookie with the expiration date
-      //document.cookie = `token_user=${token}; expires=${expirationDate.toUTCString()}; path=/;`;
-      localStorage.setItem('token', JSON.stringify(token)); // save the token to local storage
-      localStorage.setItem('userId', user._id); // save the user ID to local storage
-      localStorage.setItem('role', user.role); 
-      setIsLoggedIn(true);
-      navigate('/product');
-    })
-    .catch((error) => {
-      console.log(error);
-      toast.error("Invalid email or password.");
-    });
-};
+    axios
+      .post('http://localhost:5000/api/v1/login', { email, password })
+      .then((response) => {
+        const { token, user } = response.data;
+
+        localStorage.setItem('token', JSON.stringify(token));
+        localStorage.setItem('userId', user._id);
+        localStorage.setItem('role', user.role);
+        localStorage.setItem('address', user.address);
+        setIsLoggedIn(true);
+        navigate('/product');
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error('Invalid email or password.');
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
   if (isLoggedIn) {
     return <Navigate to="/" />;
   }
@@ -73,12 +77,15 @@ const Login = () => {
           />
         </div>
         <button type="submit" className="btn btn-primary" disabled={isLoading}>
-          {isLoading ? "Loading..." : "Login"}
+          {isLoading ? 'Loading...' : 'Login'}
         </button>
 
-        <button onClick={() => {log()}} className="btn btn-primary">
-          google
+        <button onClick={() => log()} className="btn btn-primary">
+          Google
         </button>
+        <NavLink className="nav-link" to="/ForgotPassword">
+            ForgotPassword
+        </NavLink>
       </form>
     </div>
   );
